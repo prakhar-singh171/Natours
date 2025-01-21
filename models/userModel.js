5273648ad2a364659b03863a9aef69e239cfbd2b
Prakhar Singh <prakkhar15@gmail.com>
2025-01-21 23:04:53 +0530

    Encrypted password so as to keep passwords safely stored in database

--- models/userModel.js
+++ models/userModel.js
@@ -41,6 +41,18 @@ const userSchema = new mongoose.Schema({
 });
 
 
+userSchema.pre('save', async function(next) {
+    // Only run this function if password was actually modified
+    if (!this.isModified('password')) return next();
+  
+    // Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+  
+    // Delete passwordConfirm field
+    this.passwordConfirm = undefined;
+    next();
+  });
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;

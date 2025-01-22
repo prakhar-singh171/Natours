622230f4919d45f19c59aa882ceaa02c0ea8809e
Prakhar Singh <prakkhar15@gmail.com>
2025-01-22 10:28:38 +0530

    checked if token is valid or not for user to login

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,15 +15,7 @@ const signToken = id => {
   
   const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id);
-    const cookieOptions = {
-      expires: new Date(
-        Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-      ),
-      httpOnly: true
-    };
-    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
-  
-    res.cookie('jwt', token, cookieOptions);
+   
   
     // Remove password from output
     user.password = undefined;
@@ -81,4 +73,29 @@ const signToken = id => {
         new AppError('You are not logged in! Please log in to get access.', 401)
       );
     }
+
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    // 3) Check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+      return next(
+        new AppError(
+          'The user belonging to this token does no longer exist.',
+          401
+        )
+      );
+    }
+  
+    // 4) Check if user changed password after the token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+      return next(
+        new AppError('User recently changed password! Please log in again.', 401)
+      );
+    }
+  
+    // GRANT ACCESS TO PROTECTED ROUTE
+    req.user = currentUser;
+    next();
+  
 })
--- models/userModel.js
+++ models/userModel.js
@@ -37,7 +37,8 @@ const userSchema = new mongoose.Schema({
       },
       message: 'Passwords are not the same!'
     }
-  }
+  },
+  passwordChangedAt:Date
 });
 
 
@@ -73,7 +74,20 @@ userSchema.pre('save', async function(next) {
     return await bcrypt.compare(candidatePassword, userPassword);
   };
   
-
+  userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
+    if (this.passwordChangedAt) {
+      const changedTimestamp = parseInt(
+        this.passwordChangedAt.getTime() / 1000,
+        10
+      );
+  
+      return JWTTimestamp < changedTimestamp;
+    }
+  
+    // False means NOT changed
+    return false;
+  };
+  
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;

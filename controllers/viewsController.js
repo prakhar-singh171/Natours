58860a06a93943ed920fb20e38ac3d275609d718
Prakhar Singh <prakkhar15@gmail.com>
2025-01-27 18:47:04 +0530

    added signup functionality

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -64,3 +64,9 @@ exports.updateUserData = catchAsync(async (req, res, next) => {
     user: updatedUser
   });
 });
+
+exports.getSignupForm = (req, res) => {
+  res.status(200).render('signup', {
+    title: 'Create your account'
+  });
+};
